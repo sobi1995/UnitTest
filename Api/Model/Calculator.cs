cf933b70a23b739f185378cfc54fe659fed76f4b
Sobi <sobhan.plus.plus@gmail.com>
2020-10-12 12:29:25 +0330

    add git ignore

--- Api/Model/Calculator.cs
+++ Api/Model/Calculator.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Api.Model
 {
     public class Calculator
@@ -20,8 +22,24 @@ namespace Api.Model
 
            public int Multiplication(int n1, int n2)
         {
-            return n1+n2;
+            return n1*n2;
         } 
+
+      public bool IsPrime(int n)
+    {
+        if (n > 1)
+        {
+            return Enumerable.Range(1, n).Where(x => n%x == 0)
+                             .SequenceEqual(new[] {1, n});
+        }
+
+        return false;
+    }
+
+ public bool isOdd(int number){
+            return (number % 2 !=0 );
+        }
+
     }
 
  
--- ApiXUnit/Model/CalculatorTest.cs
+++ ApiXUnit/Model/CalculatorTest.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Api.Model;
 using Xunit;
 
@@ -7,6 +8,7 @@ namespace ApiXUnit.Model
     {
 
         [Fact]
+        [Trait("Category","BaseAction")]
         public void Calculator_Test_Sum()
         {
             //Arrange
@@ -15,7 +17,6 @@ namespace ApiXUnit.Model
             n1 = 5;
             n2 = 6;
             result = 11;
-
             //Act
             int resultSum = _calculator.Sum(n1, n2);
 
@@ -24,27 +25,58 @@ namespace ApiXUnit.Model
         }
 
 
-     [Theory]
+        [Fact]
+         [Trait("Category","BaseAction")]
+        public void Calculator_Test_Division()
+        {
+            //Arrange
+            var _calculator = new Calculator();
+            int n1, n2, result;
+            n1 =50;
+            n2 =10;
+            result = 5;
+            //Act
+            int resultDivision = _calculator.Division(n1, n2);
+
+            //Assert
+            Assert.Equal(resultDivision, result);
+        }
+
+     [Fact]
+      [Trait("Category","BaseAction")]
+        public void Calculator_Test_Multiplication()
+        {
+            //Arrange
+            var _calculator = new Calculator();
+            int n1, n2, result;
+            n1 =5;
+            n2 =10;
+            result = 50;
+            //Act
+            int resultMultiplication= _calculator.Multiplication(n1, n2);
+
+            //Assert
+            Assert.Equal(resultMultiplication, result);
+        }
+
+
+        [Theory]
         [InlineData(5, 4, 9)]
-         [InlineData(9, 4, 13)]
-        [InlineData(5, 8, 9)]
+        [InlineData(9, 4, 13)]
+        [InlineData(5, 8, 13)]
+         [Trait("Category","BaseAction")]
         public void Calculator_Test_Sum_By_InlineData(int n1 ,int n2,int result)
         {
             //Arrange
             var _calculator = new Calculator();
-          
-
             //Act
             int resultSum = _calculator.Sum(n1, n2);
-
             //Assert
             Assert.Equal(resultSum, result);
         }
 
-
-
-
          [Fact]
+          [Trait("Category","BaseAction")]
         public void Calculator_Test_Sub()
         {
             //Arrange
@@ -62,5 +94,30 @@ namespace ApiXUnit.Model
         }
 
 
+    [Fact]
+ 
+        public void Calculator_Test_IsPrime()
+        {
+            //Arrange
+            var _calculator = new Calculator();
+            int n=18;
+            //Act
+            bool resultIsPrime= _calculator.IsPrime(n);
+            //Assert
+            Assert.False(resultIsPrime);
+        }
+   [Fact]
+ 
+        public void Calculator_Test_isOdd()
+        {
+            //Arrange
+            var _calculator = new Calculator();
+            int n=8;
+            //Act
+            bool resultIsisOdd= _calculator.IsPrime(n);
+
+            //Assert
+            Assert.False(resultIsisOdd);
+        }
     }
 }
